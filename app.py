9874c6c43b9c7e3f2e09cb1779ea2d9e4ff592fd
Sify1999 <arshiasabbaghi@yahoo.com>
2025-10-22 20:31:59 +0330

    Fixed link extraction and improved quality matching

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from setting import Setting
 # Enter the idm path here ...
 idm_path = r"C:\Program Files (x86)\Internet Download Manager\IDMan.exe"
 setting = Setting()
+
+# translator
+def persian_to_english(text):
+    translation = {
+        "فصل" : "season",
+        "پخش آنلاین" : " online"
+    }
+    for persian_word , english_word in translation.items():
+        text = text.replace(persian_word , english_word)
+    return text
 # read html
 def readHtml(filename):
     with open(filename , "r" , encoding="utf-8") as f:
@@ -28,7 +38,8 @@ def reset_idm_queue():
 
     # delete all download then minimize
     print("Deleting Downloads...")
-    pyautogui.hotkey('ctrl' , 'a' , 'delete')
+    pyautogui.hotkey('ctrl' , 'a')
+    pyautogui.press('delete')
     time.sleep(0.5)
     print("Minimizing...")
     pyautogui.hotkey('alt' , 'space')
@@ -40,30 +51,34 @@ def extract_links(seasons):
     with open("links.txt", "w", encoding="utf-8") as f:
         i = 1
         for season in seasons:
-            season_num = season.find("h3").text.strip()
-            quality = season.find("div", class_="head_left_side").text.strip()
+            season_num = persian_to_english(season.find("h3").text.strip())
+            quality = persian_to_english(season.find("div", class_="head_left_side").text.strip())
             links = [a["href"] for a in season.select(".part_item a.partlink[href]") if a["href"] != "#"]
-            if quality.find(setting.quality) != -1:
-                f.write(f"{season_num} - {quality}\n")
+            text = f"{season_num} ==> {quality}"
+            if text == setting.quality:
                 for link in links:
-                    f.write(str(i) +  ". " + link +"\n")
                     print(f"{i}.Link Extracted ")
                     if setting.idm_download:
                         subprocess.run([idm_path , "/d" , link , "/a" , "/n"])
                     i += 1
-                f.write("\n")
-
+            f.write(f"{season_num} - {quality}\n")
+            for j , link in enumerate(links , start=1):
+                f.write(str(j) +  ". " + link +"\n")
+            f.write("\n")
 
 soup = readHtml("file.html")
 qualities = []
 
-
 #extracts all quality names
 seasons = soup.find_all("div" , class_="item_row_series parent_item")
 for quality_div in seasons:
-    quality = quality_div.find("div" , class_="head_left_side").text.strip()
-    qualities.append(quality)
+    season_name = persian_to_english(quality_div.find("h3").text.strip())
+    quality = persian_to_english(quality_div.find("div" , class_="head_left_side").text.strip())
+    text = f"{season_name} ==> {quality}"
+    if text.find("online") == -1:
+        qualities.append(text)
 print('\n'.join(qualities))
+    
 while True:
     user_quality = input("Enter the Quality you want : ")
     if user_quality in qualities:
@@ -87,7 +102,8 @@ if user_start_download.lower() in ["yes" , "true" , "y" , "1"]:
 extract_links(seasons)
 if setting.idm_start :
     subprocess.run([idm_path , "/s"])
-elif setting.idm_download :
+if setting.idm_download :
     subprocess.run(idm_path)
 print(setting)
+print(f"\"{user_quality}\"")
 setting.resetSetting()
